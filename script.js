ae72dbc3c5ec077b1b363acab95f0eb71a58e3b2
Nicholas Langley <w0106967@myscc.ca>
2022-09-16 18:39:23 -0400

    Completed assignment1
    Added change event to the input boxes to calculate the money available to spend based on the amounts input.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Your Name Here}
+	Nicholas Langley
+	0106967
 */
-
+$(function(){
+	$('input').change(function(){
+	let $amount = $('#amount');
+	let $salary = $('#yearly-salary').val();
+	let $spendPercent = $('#percent').val();
+	$amount.text('$' + ($salary * ($spendPercent / 100)).toFixed(2));
+	});
+});
