a2e5668e6b4c738a1e5445aa42ec20e5536db175
langleynick <w0106967@myscc.ca>
2022-10-05 21:38:04 -0400

    Added onclick functions

--- WEB303As02/js/main.js
+++ WEB303As02/js/main.js
@@ -1,3 +1,50 @@
 // WEB303 Assignment 2
+// Programmed by: Nicholas Langley
 
-
+$(function(){
+    $('#prospect').on('click', function()
+    {
+        $('#content').hide();
+        let xhr = new XMLHttpRequest;
+        xhr.open('GET', 'prospect.html', true);
+        xhr.onload = function()
+        {
+            if(this.status===200) 
+            {
+                let content = document.getElementById("content");
+                content.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    });
+    $('#convert').on('click', function()
+    {
+        $('#content').hide();
+        let xhr = new XMLHttpRequest;
+        xhr.open('GET', 'convert.html', true);
+        xhr.onload = function()
+        {
+            if(this.status===200)
+            {
+                let content = document.getElementById("content");
+                content.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    });
+    $('#retain').on('click', function()
+    {
+        $('#content').hide();
+        let xhr = new XMLHttpRequest;
+        xhr.open('GET', 'retain.html', true);
+        xhr.onload = function()
+        {
+            if(this.status===200)
+            {
+                let content = document.getElementById("content");
+                content.innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    });
+});
