60861b785e65f2e7db3276a7526ad07c11bd6fe7
langleynick <w0106967@myscc.ca>
2022-10-20 22:14:47 -0400

    Added javascript to get location and implement local storage

---
+++ WEB303As04/js/script.js
@@ -0,0 +1,64 @@
+/*
+    Assignment #4
+    {Your name here}
+*/
+
+$(function () {
+    // your code here
+    const location = document.getElementById("locationhere");
+    try{
+        navigator.geolocation.getCurrentPosition(success, fail);
+    } catch{
+        x.innerHTML = err;
+    }
+    function success(position){
+        lat1 = position.coords.latitude;
+        lon1 = position.coords.longitude;
+        let sessionLocation = document.createElement("p");
+        sessionLocation.innerHTML = "Longitude: " + lon1 + " Latitude: " + lat1;
+        location.before(sessionLocation);
+    };
+    function fail(){
+        alert("You must enable Location Services to continue with this application");
+    };
+    if(localStorage.getItem('lat2')){
+        lat2 = JSON.parse(window.localStorage.getItem('lat2'));
+        lon2 = JSON.parse(window.localStorage.getItem('lon2'));
+        let oldLocation = document.createElement("p");
+        oldLocation.innerHTML = "Old Longitude: " + lon2 + " Old Latitude: " + lat2;
+        location.append(oldLocation);
+        let welcomeBack = document.createElement("h1");
+        welcomeBack.innerHTML = "Welcome Back!";
+        location.append(welcomeBack);
+        let meterDistance = calcDistanceBetweenPoints();
+        let kmDistance = meterDistance / 100;
+        welcomeBack.after(kmDistance + "km");
+    } 
+    if (!localStorage.getItem('lat2')){ 
+        location.append("Welcome!");
+    }
+        window.localStorage.setItem('lat2', JSON.stringify(lat1));
+        window.localStorage.setItem('lon2', JSON.stringify(lon1));
+    
+    // DO NOT EDIT ANY CODE IN THIS FUNCTION DEFINTION
+    // function to calculate the distance in metres between two lat/long pairs on Earth
+    // Haversine formula - https://en.wikipedia.org/wiki/Haversine_formula
+    // Aren't those cool variable names? Yah gotta love JavaScript
+    function calcDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
+        var toRadians = function (num) {
+            return num * Math.PI / 180;
+        }
+        var R = 6371000; // radius of Earth in metres
+        var φ1 = toRadians(lat1);
+        var φ2 = toRadians(lat2);
+        var Δφ = toRadians(lat2 - lat1);
+        var Δλ = toRadians(lon2 - lon1);
+
+        var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (R * c);
+    }
+});
+
+
