35668eae00186c5c96415594af1751f4c545eeda
langleynick <w0106967@myscc.ca>
2022-10-27 23:48:37 -0400

    Fixed loop
    Added the counter parameter into the for loop to display only the array value at the desired index

--- WEB303As05/script.js
+++ WEB303As05/script.js
@@ -11,33 +11,42 @@ $(document).ready(function () {
         category;
 
         constructor(id, name, description, category){
-            this.id=id;
-            this.name=name;
-            this.description=description;
-            this.category=category;
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.category = category;
         }
 
         updateContentItem (id, name, description, category) {
-            if (this.id == id && name, description, category !== null){
-                name=this.name;
-                description=this.description;
-                category=this.category;
+            if (id == this.id && name, description, category !== null){
+                this.name = name; 
+                this.description = description;
+                this.category = category;
             } else {
                 return;
             }
-        }
+            }
         toString (){
-            return '<div class="content-item-wrapper" id="content-item-' + this.ID + '"><h2>Name: ' + this.name + '</h2><br><p>Description: ' + this.description + '</p><br><div>Category: ' + this.category + '</div></div>';
+            return '<div class="content-item-wrapper" id="content-item-' + this.id + '"><h4>Name: ' + this.name + '</h4><p>Description: ' + this.description + '</p><div>Category: ' + this.category + '</div></div>';
         }
     }
-    let steelers = new ContentItem(1, "Steelers", "Football Team out of Pittsburgh, PA", "NFL");
-    let tigers = new ContentItem(2, "Tigers", "Baseball Team out of Detroit, MI", "MLB");
-    let lakers = new ContentItem(3, "Lakers", "Basketball Team out of Los Angeles, CA", "NBA");
-    let lancers = new ContentItem(4, "Lancers", "Football Team out of Windsor, ON", "OUA");
-    let redWings = new ContentItem(5, "Red Wings", "Hockey Team out of Detroit, MI", "NHL");
+    let steelers = new ContentItem(0, "Steelers", "Football Team out of Pittsburgh, PA", "NFL");
+    let tigers = new ContentItem(1, "Tigers", "Baseball Team out of Detroit, MI", "MLB");
+    let lakers = new ContentItem(2, "Lakers", "Basketball Team out of Los Angeles, CA", "NBA");
+    let lancers = new ContentItem(3, "Lancers", "Football Team out of Windsor, ON", "OUA");
+    let redWings = new ContentItem(4, "Red Wings", "Hockey Team out of Detroit, MI", "NHL");
 
-    for (let i = 0; i < ContentItem.length; i++){
-        $('div#content-item-list').append(team.toString());
+    let array = [steelers, tigers, lakers, lancers, redWings];
+
+    for (let i = 0; i <= array.length; i++){
+        $('div#content-item-list').append(array[i].toString());
+        $('div.content-item-wrapper').css({
+            "border" : "1px solid red",
+            "width" : "80%",
+            "padding" : "15px",
+            "margin" : "0 auto",
+            "margin-top" : "10px"
+        });
     }
 });
 
