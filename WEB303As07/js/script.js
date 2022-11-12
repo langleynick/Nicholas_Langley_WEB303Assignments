e850e9c1add612f771431dd9d32a1d4dc7fcb5a7
langleynick <w0106967@myscc.ca>
2022-11-11 21:55:47 -0500

    Finished assignment
    added in the photo viewer and the modal script, TODO: resolve how to get the header and the close button to display

--- WEB303As07/js/script.js
+++ WEB303As07/js/script.js
@@ -1,11 +1,12 @@
 $(function () {
     $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
-        var $content = $(this).clone().find('img').css({
-            marginLeft: 0,
-            marginTop: 0,
-            width: '100%',
-            height: 'auto'
-        });
-        //modal code goes here
+            e.preventDefault();
+            var $content = $(this).clone().find('img').css({
+                marginLeft: 0,
+                marginTop: 0,
+                width: '100%',
+                height: 'auto'
+            });
+            modal.open({content: $content, width: 800, height: 450});
     });
-});
+    }());
