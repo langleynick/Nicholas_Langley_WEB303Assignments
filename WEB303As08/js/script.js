bf632551a2530bed55a7e226e2a9fa9585c7ac69
langleynick <w0106967@myscc.ca>
2022-11-24 23:39:32 -0500

    Final commit
    Added click function to sort table when header is clicked.
    First click sorts in ascending
    Second click sorts in descending
    Third click resets the table back to original format
    Each click changes content in the header element, adding an arrow to match how the data is sorted,

--- WEB303As08/js/script.js
+++ WEB303As08/js/script.js
@@ -2,8 +2,8 @@ $(function(){
     $.ajax({
         url: "data/characters.json",
         success: function(results){
-            var $table = $("<table />");
-            var $headers = $("<tr><th>Alias&nbsp;</th><th>First Name&nbsp;</th><th>Last Name&nbsp;</th><th>Actor&nbsp;</th><th>Date First Appeared(Comics)</th></tr>")
+            var $table = $("<table class='sortable' />");
+            var $headers = $("<thead><tr><th data-sort='name'>Alias</th><th data-sort='name'>First Name</th><th data-sort='name'>Last Name</th><th data-sort='name'>Actor</th><th data-sort='date'>Date First Appeared(Comics)</th></tr></thead>")
             $table.append($headers);
             var $tableBody = $('<tbody></tbody>');
             $table.append($tableBody);
@@ -12,11 +12,70 @@ $(function(){
                 var $row = $('<tr><td>' + character.alias + '</td><td>' + character.firstName + '</td><td>' + character.lastName + '</td><td>' + character.actor + '</td><td>' + character.dateFirstAppeared + '</td></tr>');
                 $tableBody.append($row);
             }
-        $('#table').append($table);
+        $('h1').append($table);
+        var compare = {
+            name: function (a,b) {
+                a = a.replace(/^the /i, '');
+                b =  b.replace(/^the /i, '');
+        
+                if (a < b) {
+                    return -1;
+                } else {
+                    return a > b ? 1 : 0;
+                }
+            },
+            duration: function (a, b){
+                a = a.split(':');
+                b = b.split(':');
+        
+                a = Number(a[0])* 60 + Number(a[1]);
+                b = Number(b[0])* 60 + Number(a[1]);
+        
+                return a - b;
+            },
+            date: function (a, b) {
+                a = new Date(a);
+                b = new Date(b);
+        
+                return a - b;
+            }
+           };
+           $('.sortable').each(function(){
+            var $table = $(this);
+            var $tbody = $table.find('tbody');
+            var $controls = $table.find('th');
+            var rows = $tbody.find('tr').toArray();
+            var originalRows = $tbody.find('tr');
+
+            $controls.on('click', function(){
+                var $header = $(this);
+                var order = $header.data('sort');
+                var column;
+        
+                if($header.is('.ascending')) {
+                    $header.removeClass('ascending');
+                    $header.addClass('descending');
+                    $tbody.append(rows.reverse());
+                } else if($header.is('.descending')){
+                    $header.removeClass('descending');
+                    $tbody.append(originalRows);
+                } else {
+                    $header.addClass('ascending');
+                    $header.siblings().removeClass('ascending descending');
+        
+                    if(compare.hasOwnProperty(order)) {
+                        column = $controls.index(this);
+                        rows.sort(function(a,b){
+                            a = $(a).find('td').eq(column).text();
+                            b = $(b).find('td').eq(column).text();
+                            return compare[order](a,b);
+                        });
+                        $tbody.append(rows);
+                    }
+                }
+            })
+           })
         }
     });
     //TODO: ADD SORTING FUNCTIONS FOR EACH COLUMN
-    $('tbody tr th').each(function(){
-        
-    });
-});
+})
